48ec5773d60e0ebe45a1729ea9e9b402e83f0f1e
Babylon182 <guido_calonge@hotmail.com>
2018-10-29 21:59:00 -0300

    First Milestone: - Hero movement - Basic spawning system - Basic enemy - Basic weapon

---
+++ Assets/Scripts/Entity/EnemyMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : EntityMovement
+{
+    public override void Move()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
---
+++ Assets/Scripts/Entity/EntityMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class EntityMovement : MonoBehaviour
+{
+    [SerializeField] protected float speed; 
+    protected Vector3 direction;
+
+    public abstract void Move();
+    
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+}
